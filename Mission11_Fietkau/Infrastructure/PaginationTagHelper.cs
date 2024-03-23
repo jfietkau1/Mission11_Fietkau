7c5df757343409105fd51ef14066e3fa11df1312
jfietkau1 <120548552+jfietkau1@users.noreply.github.com>
2024-03-23 00:27:05 -0600

    buttons

--- Mission11_Fietkau/Infrastructure/PaginationTagHelper.cs
+++ Mission11_Fietkau/Infrastructure/PaginationTagHelper.cs
@@ -22,6 +22,14 @@ namespace Mission11_Fietkau.Infrastructure
         public ViewContext? ViewContext { get; set; }
         public string? PageAction { get; set; }
         public PaginationInfo PageModel { get; set; }
+
+        public bool PageClassEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -35,6 +43,12 @@ namespace Mission11_Fietkau.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
 
+
+                    if (PageClassEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal); 
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
 
